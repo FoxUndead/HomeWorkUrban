78f9f9e48fe7f5d970bbdc32b5b6ff3afb3c5abe
FoxUndead <70019379+FoxUndead@users.noreply.github.com>
2024-07-23 20:52:38 +0300

    Add files via upload

---
+++ Popov1.1-4.py
@@ -0,0 +1,14 @@
+# 1st program
+print('1st program')
+print(int(9 ** 0.5 * 5))
+# 2nd program
+print('2nd program')
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print('3nd program')
+print("Даны числа 1234 и 5678")
+print(int((1234-1004+5678-5008)/10))
+# 4th program
+print('4th program')
+print('Даны числа 13.42 и 42.13')
+print(((int(13.42 / 1)) == (int(42.13 * 100 - 4200))) or ((int(42.13 / 1)) == (int(13.42 * 100 - 1300))))
