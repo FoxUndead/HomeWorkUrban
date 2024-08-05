7366efb6c8e785fd8a9176ad3eb21ab1e87c103a
FoxUndead <70019379+FoxUndead@users.noreply.github.com>
2024-08-05 19:39:12 +0300

    Add files via upload

---
+++ Popov. Строки и индексация строк.py
@@ -0,0 +1,17 @@
+#Не понял какое должно быть слово
+example = "Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
+example2 = "Urban"
+print(example2[0])
+print(example2[-1])
+print(example2[2:])
+print(example2[::-1])
+print(example2[1::2])
+
+
+
+
