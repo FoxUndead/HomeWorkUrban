3cf2d541d2054bca98839620516709030157f830
FoxUndead <70019379+FoxUndead@users.noreply.github.com>
2024-08-08 18:41:27 +0300

    Add files via upload

---
+++ Popov. Переменные.py
@@ -0,0 +1,5 @@
+Number_of_home_works = 12
+Spent_time_hours = 1.5
+Course_name = 'Python'
+Spent_time_one = Spent_time_hours / Number_of_home_works
+print("Курс:", Course_name, ", всего задач - ", Number_of_home_works, ", затрачено часов - ", Spent_time_hours, ", среднее время выполнения - ", Spent_time_one, "часа.")
