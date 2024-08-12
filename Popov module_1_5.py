3b53873c7747d9a2c92d1b4c3ed92cf1f34f8025
FoxUndead <70019379+FoxUndead@users.noreply.github.com>
2024-08-12 17:43:21 +0300

    Add files via upload

---
+++ Popov module_1_5.py
@@ -0,0 +1,7 @@
+mutable_list = [1, 2, 'sh', 'it', True]
+mutable_list[3] = 'ow'
+print(mutable_list)
+immutable_var = tuple([1, 2, 'sh', 'it', True])
+print(immutable_var)
+# tuple[3] = "ow"
+# Нельзя изменить, потому что такой тип данных
